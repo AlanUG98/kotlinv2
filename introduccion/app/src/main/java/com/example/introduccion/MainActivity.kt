a702f75c2f36cb6589c504f171e04fe0eea9c3dc
Alan Urbina <alan.urga@gmail.com>
2024-10-04 21:43:43 -0600

    introduccion a kotlin, primer capitulo

--- introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
+++ introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
@@ -16,32 +16,7 @@ import com.example.introduccion.ui.theme.IntroduccionTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            IntroduccionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        println ("heello world")
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntroduccionTheme {
-        Greeting("Android")
-    }
-}
